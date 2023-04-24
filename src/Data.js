287ece4f736c75672f21f4108599fb5f5d31ec5f
Simran-31 <100121968+Simran-31@users.noreply.github.com>
2023-04-25 00:28:42 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Smart Phone",
+    price: 18999,
+    desc: "OnePlus Nord CE 2 Lite 5G (Blue Tide, 6GB RAM, 128GB Storage).",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Sneakers",
+    price: 1379,
+    desc: "Bacca Bucci Men's Running Shoe",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Smart Watch",
+    price: 1799,
+    desc: "Amazing Unbound NEO 1.8\" Super AMOLED 2.5D Curved Display, One-Tap BT Calling Smartwatch (Black)",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Wall Mirror",
+    price: 4799,
+    desc: "TIED RIBBONS Wall Mirror Decorative Mounted Hanging Metal Sunflower Design Framed Round Mirror for Home Decor Living Room Bedroom Bathroom Wash Basin Drawing Room Decoration Items (Multi, 76.2 cm)",
+    img: "images/img-4.jpg",
+  },
+  {
+    id: "kokjfhgbvnscs",
+    name: "Study Table",
+    price: 4999,
+    desc: "Plantex DIY Study Table/Laptop Table/Desk for Office/Home/Classroom/K-Leg Foldable Table-Workstation (Walnut Textured, APS-1000)",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "jijioytrhndcv",
+    name: "HP Laptop",
+    price: 40799,
+    desc: "HP 15s, 11th Gen Intel Core i3 8GB RAM/1TB HDD+256 GB SSD 15.6-inches/39.6 cm FHD Laptop/Windows 11/Intel UHD Graphics/Dual Speakers/Alexa/MSO/Fast Charge",
+    img: "images/img-6.jpg",
+  },
+  {
+    id: "ujwuefbncxbsn",
+    name: "Decorative Item",
+    price: 799,
+    desc: "PREK Decorative Items for Home Decor Item Seven Chakra Crystal Tree Quirky Home Decor Showpieces for Good Luck Money Crystal Tree Gift Items Room Decor Wedding Gift Standard Size - Multicolor, 1 Piece",
+    img: "images/img-7.jpg",
+  },
+  {
+    id: "tjuvhyfhcbcv",
+    name: "Painting",
+    price: 42099,
+    desc: "Painting Mantra - Multicolor Jardine Birds Set of 4 Brown Framed Painting,UV Textured Art Prints (10 x 10 inch)",
+    img: "images/img-8.jpg",
+  },
+];
